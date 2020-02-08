385c3dd55f71d3660beba9f7a1306a94f99f6f14
bat-fleck <stanbrooks07@gmail.com>
2020-02-08 12:21:53 +0530

    fixed bms

--- bms.py
+++ bms.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 import json
 import requests
@@ -7,18 +7,29 @@ import sys
 from selenium.webdriver.common.keys import Keys
 
 
-testurl = "https://in.bookmyshow.com/chennai/movies/ford-v-ferrari/ET00104241/user-reviews"
-XPATH_loadmore = "//*[@id='load-more-trigger']"
-XPATH_grade = "//*[@class='review-container']/div[1]"
-list_grades = []
+testurl = "https://in.bookmyshow.com/ahmedabad/movies/jawaani-jaaneman/ET00104334/user-reviews"
 
 driver = webdriver.Firefox()
 driver.get(testurl)
 
 last_height = driver.execute_script("return document.body.scrollHeight")
 
-for i in range(20): # adjust integer value for need
+for i in range(60):    
+     # adjust integer value for need
+    # driver.sendKeys(Keys.DOWN);
+    driver.find_element_by_tag_name('body').send_keys(' ')
     # you can change right side number for scroll convenience or destination 
-    driver.execute_script("window.scrollBy(0, 250)")
+    # driver.execute_script("window.scrollBy(0, 250)")
     # you can change time integer to float or remove
-    time.sleep(1)
+    time.sleep(0.8)
+
+list_content=[]
+soup = bs(driver.page_source, features="html.parser")
+content = soup.find_all('div', class_=['text','__reviewer-text'])
+list_content = [tag.get_text() for tag in content]    
+print(list_content)
+
+with open('newy.txt', 'w') as f:
+    for item in list_content:
+        f.write("%s\n" % item)
+driver.quit()
