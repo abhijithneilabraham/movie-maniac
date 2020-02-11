80bdfeb01ff0acff40fd1cb591b408e0a9552070
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-02-12 00:35:50 +0530

    updates in  html and saving matplotlib image

--- final_version/combined.py
+++ final_version/combined.py
@@ -10,6 +10,7 @@ import nltk
 from nltk import tokenize
 import numpy as np
 from textblob import TextBlob 
+import matplotlib.pyplot as plt
 
 def scraper(fileName,imdbUrl,rtUrl,bsmUrl,num):
     print("""
@@ -129,25 +130,55 @@ def scraper(fileName,imdbUrl,rtUrl,bsmUrl,num):
     sentences=tokenize.sent_tokenize(read_file) #tokenization means splitting into meaningful stuff,like,splitting into words.
     number_of_sentences=len(sentences)
     print("Total sentences =",number_of_sentences)
-    if number_of_sentences>0:
+    total=0
+    pos=0
+    neg=0
+    neutral=0
+    allWords = nltk.tokenize.word_tokenize(read_file)
+    stopwords =set( nltk.corpus.stopwords.words('english'))
+    allWordExceptStopDist = nltk.FreqDist(w.lower() for w in allWords if w.lower() not in stopwords)   
+    mostCommon= allWordExceptStopDist.most_common(10)
+    garb=[".",",","'s","n't","!"]
+    mostCommon=[i for i in mostCommon if i[0] not in garb]
+    
+    print("The most commonly used words along with their frequency count")
+    for i in mostCommon:
+        print("word =",i[0],"frequency =",i[1])
         
-        total=0
-        for p in sentences: 
-            q=TextBlob(p)
-            senti=q.sentiment 
-            '''
-            to understand what TextBlob.sentiment does,here is an example.
-            TextBlob("not a very great calculation").sentiment
-        gives the result=Sentiment(polarity=-0.3076923076923077, subjectivity=0.5769230769230769)
+    for p in sentences: 
+        q=TextBlob(p)
+        senti=q.sentiment 
+        
         '''
-            total=total+senti.polarity #We only want the polarity here.So we summed it up over for a sentence
-            
-        
-        average=total/number_of_sentences #total polarity /number of sentences gives an average polarity.
-    else:
-        print("Not enough reviews generated to do sentiment analysis.")
-    if average>0:
-        return "positive"
-    else:
-        return "negative"  
+        to understand what TextBlob.sentiment does,here is an example.
+        TextBlob("not a very great calculation").sentiment
+     gives the result=Sentiment(polarity=-0.3076923076923077, subjectivity=0.5769230769230769)
+     '''
+        total=total+senti.polarity #We only want the polarity here.So  summed it up over for sentences.
+        if(senti.polarity==0):
+            neutral+=1
+        elif(senti.polarity<0):
+            neg+=1
+        else:
+            pos+=1
+    #average=total/number_of_sentences #total polarity /number of sentences gives an average polarity.
+    #
+    #print("average polarity =",average*100,"%")
+    
+    
+    
+    # Data to plot
+    labels = 'Positive', 'Negative', 'Neutral'
+    sizes = [pos, neg, neutral]
+    colors = ['gold', 'yellowgreen', 'lightcoral']
+    explode = (0.1, 0, 0)  # explode 1st slice
+    
+    # Plot
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+    autopct='%1.1f%%', shadow=True, startangle=140)
+    
+    plt.axis('equal')
+    plt.savefig('sentiment.png')
+    plt.show()
+
 
